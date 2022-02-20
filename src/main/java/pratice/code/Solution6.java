c8f313464fb21e6949f56a41ea565892c6a40dd0
hyejin1231 <phj981231@naver.com>
2022-02-21 08:47:48 +0900

    20220221 프로그래머스 레벨 1 나머지가 1이 되는 수 찾기

---
+++ src/main/java/pratice/code/Solution6.java
@@ -0,0 +1,26 @@
+package pratice.code;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        Solution6 solution6 = new Solution6();
+        solution6.solution(10); // result = 3
+//        solution6.solution(12);
+    }
+
+    public int solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = n-1; i >= 1; i--) {
+            if (n % i == 1) {
+                list.add(i);
+            }
+        }
+
+        Collections.sort(list);
+
+        return list.get(0);
+    }
+}
