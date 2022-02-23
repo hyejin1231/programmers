6bf28e54f2649d737fe08e27e1068ef577fdf977
hyejin1231 <phj981231@naver.com>
2022-02-23 09:06:11 +0900

    20220223 프로그래머스 레벨 1 최소 직사각형

---
+++ src/main/java/pratice/code/Solution8.java
@@ -0,0 +1,34 @@
+package pratice.code;
+
+
+import static java.lang.Math.*;
+
+public class Solution8 {
+    public static void main(String[] args) {
+        Solution8 solution8 = new Solution8();
+        int[][] sizes= {
+                {60,50},
+                {30,70},
+                {60,30},
+                {80,40}
+        };
+        int solution = solution8.solution(sizes);
+        System.out.println(solution);
+    }
+
+    // 최소 직사각형 가로, 세로
+    public int solution(int[][] sizes) {
+        int widthMax = 0;
+        int heightMax = 0;
+
+        for (int i = 0; i < sizes.length; i++) {
+            int w = max(sizes[i][0], sizes[i][1]);
+            int h = min(sizes[i][0], sizes[i][1]);
+
+            widthMax = max(widthMax, w);
+            heightMax = max(heightMax, h);
+        }
+
+        return widthMax * heightMax;
+    }
+}
