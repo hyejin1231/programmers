bdb7323ea64a01b1fe9a0fa8c72b25362d782760
hyejin1231 <phj981231@naver.com>
2022-03-16 00:16:20 +0900

    20220315 프로그래머스 Level 1 [1차] 다트게임

--- src/main/java/pratice/code/level1/Solution22.java
+++ src/main/java/pratice/code/level1/Solution22.java
@@ -1,5 +1,7 @@
 package pratice.code.level1;
 
+import java.util.ArrayList;
+
 public class Solution22 {
     public static void main(String[] args) {
         Solution22 solution22 = new Solution22();
@@ -12,6 +14,46 @@ public class Solution22 {
     // [1차] 다트 게임
     public int solution(String dartResult) {
         int answer = 0;
+        String first = "";
+        int[] list = new int[3];
+        int idx= 0;
+
+        for (int i = 0; i < dartResult.length(); i++) {
+
+            switch (dartResult.charAt(i)) {
+                case '*':
+                    list[idx-1] *= 2;
+                    if (idx > 1) list[idx-2] *= 2;
+                    break;
+                case '#':
+                    list[idx-1] *= -1;
+                    break;
+                case 'S':
+                     list[idx] = (int) Math.pow(Integer.parseInt(first), 1);
+                     idx++;
+                    first = "";
+                    break;
+                case 'D':
+                    list[idx] = (int) Math.pow(Integer.parseInt(first), 2);
+                    idx++;
+                    first = "";
+                    break;
+                case 'T':
+                    list[idx] = (int) Math.pow(Integer.parseInt(first), 3);
+                    idx++;
+                    first = "";
+                    break;
+                default:
+                    first += String.valueOf(dartResult.charAt(i));
+                    break;
+            }
+        }
+
+        for (int i : list) {
+            answer += i;
+        }
+
+
         return answer;
     }
 
