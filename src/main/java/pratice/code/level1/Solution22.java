4f7b08c82ff87b40fec77e6d72caa2743f2c7797
hyejin1231 <hyejinbag772@gmail.com>
2022-03-15 15:40:50 +0900

    202203015 프로그래머스 level1 다트게임 (해야함_)

---
+++ src/main/java/pratice/code/level1/Solution22.java
@@ -0,0 +1,18 @@
+package pratice.code.level1;
+
+public class Solution22 {
+    public static void main(String[] args) {
+        Solution22 solution22 = new Solution22();
+        System.out.println(solution22.solution("1S2D*3T")); // result 37 11 * 2 + 22 * 2 + 33
+        System.out.println(solution22.solution("1D2S#10S")); // result 9 12 + 21 * (-1) + 101
+        System.out.println(solution22.solution("1D2S0T")); // result 3 12 + 21 + 03
+    }
+
+
+    // [1차] 다트 게임
+    public int solution(String dartResult) {
+        int answer = 0;
+        return answer;
+    }
+
+}
