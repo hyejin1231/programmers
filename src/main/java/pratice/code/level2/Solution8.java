f5251b9bde71017b35dd5f772de1d1f0687ed4bb
hyejin1231 <hyejinbag772@gmail.com>
2022-04-01 15:17:16 +0900

    20220401 프로그래머스 level2 N개의 최소공배수

--- src/main/java/pratice/code/level2/Solution8.java
+++ src/main/java/pratice/code/level2/Solution8.java
@@ -6,16 +6,32 @@ public class Solution8 {
         Solution8 solution8 = new Solution8();
         int[] arr = {2,6,8,14};
         int[] arr2 = {1, 2, 3};
-
         System.out.println(solution8.solution(arr)); // result 168
         System.out.println(solution8.solution(arr2)); // result 6
     }
 
     // N개의 최소공배수
     public int solution(int[] arr) {
-        int answer = 0;
+        int max = arr[0];
 
-        return answer;
+        for (int i : arr) {
+            if (max < i) max = i;
+        }
+
+        for (int i = 1; ; i++) {
+            int temp = max * i;
+            boolean flag = true;
+
+            for (int j = 0; j < arr.length; j++) {
+                if (temp % arr[j] != 0) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) return temp;
+        }
+
     }
 
 
