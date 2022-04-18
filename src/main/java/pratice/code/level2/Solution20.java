435e64f4ed0fef3112ecbc5293ba3d93ed1f3361
hyejin1231 <hyejinbag772@gmail.com>
2022-04-19 08:08:30 +0900

    20220419 프로그래머스 level2 티켓 넘버(진행중..)

---
+++ src/main/java/pratice/code/level2/Solution20.java
@@ -0,0 +1,39 @@
+package pratice.code.level2;
+
+public class Solution20 {
+    public static void main(String[] args) {
+        Solution20 solution20 = new Solution20();
+        int[] numbers = {1, 1, 1, 1, 1};
+        int[] numbers2 = {4, 1, 2, 1};
+        System.out.println(solution20.solution(numbers, 3)); // result 5
+        System.out.println(solution20.solution(numbers2, 4 )); // result 2
+    }
+
+    // 티켓 넘버 - 깊이/너비 우선
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+
+        return answer;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
